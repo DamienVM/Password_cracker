b5271720bc83dc3d3c057c11200176958dce8d53
DamienVM <damien.vanmeerbeeck@student.uclouvain.be>
2019-04-28 16:27:41 +0200

    Mise à jour

--- cracker.c
+++ cracker.c
@@ -7,30 +7,53 @@
 #include <unistd.h>
 #include <errno.h>
 
-
-
+char **ftrad;        /*Liste des fichiers dont le premier argument est un pointeur vers le nbr de fichiers*/
+char **hash;         /*Buffer pour les pointeurs hash */
+pthread_mutex_t mutex_hash;
+int N =1;
 /*
 Fonction pour le thread de lecture
  */
-void *lecture(void* ptr)
+void *lecture(void* param)
 {
-  char *param = (char*)ptr;
-  int nf = (int)param[0]; 
+  int nf = (int)*ftrad[0];
   printf("il y a %i fichiers a lire\n",nf);
   int count =0;
-  uint32_t* buff;
+  char* buff = malloc(32);
+
   for(int i=1;i<nf+1;i++){
-    int a = open(&param[i],O_RDONLY);
-    if(a==-1){
-      printf("erreur1\n");
-    }
+    printf("%s\n",ftrad[i]);
+    int a = open(ftrad[i],O_RDONLY);
+    if(a==-1){printf("erreur1\n"); }
+
     int stat = 1;
-    while(stat!=0){
-      stat = read(a,(void*) buff,sizeof(uint32_t*));
-      count++;
+    while(stat  != 0){
+      stat = read(a,buff,32);
+      /*printf("%064x\n",*buff);*/
       if (stat==-1){
 	printf("erreur2\n");
+	printf("%\n ",strerror(errno));
 	exit(0);
+      }
+      else if(stat != 0){
+	for(int i =0; i==0;){
+	  pthread_mutex_lock(&mutex_hash);
+	  for(int j = 0; j < N+1 && i==0 ;j++){
+	    if(hash[j]==NULL){
+	      hash[j]=buff;
+	      printf("\t copié en zone %i\n",j);
+	      i=1;
+	    }
+	    else if(j==N){
+	      i=1;
+	    }
+	  }
+	  
+	  pthread_mutex_unlock(&mutex_hash);
+	}
+	
+	count++;
+	/*printf("\t %i %064hhx \n",count,buff);*/
       }
     }
     close(a);
@@ -46,7 +69,6 @@ Fonction principale
 int main(int argc,char *argv[])
 {
   printf("\033[0;34mFonction principale commencée\033[00m\n");
-  int N = 1;     /*nombre de threads de calculs*/
   int c = 0;     /*Consonne ativé*/
   int nargs = 1; /*Compteur d'arguments*/
   int o = 0;     /*Fichier de sortie ou pas*/
@@ -82,23 +104,26 @@ int main(int argc,char *argv[])
   printf("\033[0;31mArivée à la prochaine étape\033[00m\n");
 
   int nf = argc-nargs; /*Nombre de fichiers en argument*/
-  char* ftrad[nf+1];     /*Liste des fichiers dont le premier argument est un pointeur vers le nbr de fichiers*/ 
   printf("il y a %i fichiers à traiter\n",nf);
-  char* cnf = (char*)&nf; 
-  ftrad[0]= cnf;
+
+  ftrad=  malloc(sizeof(char*)*(nf+1));
+  ftrad[0] = (char*)&nf;
+
   printf("il y a bien %i a traiter\n",(int)*ftrad[0]) ;
   for(int i = 1; i<nf+1 ;i++)
     {
       ftrad[i]=argv[nargs+i-1];
-      printf("\tfichier %i copié %s \n",i,ftrad[i]);
+      printf("\tfichier %i copié %s\n",i,ftrad[i]);
     }
   
   printf("creation du tableau de buffer\n"); 
-  uint8_t**ptr = malloc(sizeof(uint8_t)*(N+1)); /*Buffer pour les pointeurs hash */
-  if(ptr[0]==NULL)
+  hash = calloc((N+1),sizeof(uint8_t));
+  if(hash[0]==NULL)
     {
       printf("\tfait\n");
     }
+  pthread_mutex_init(&mutex_hash,NULL);
+  
 
 
 
@@ -110,14 +135,16 @@ int main(int argc,char *argv[])
   
   pthread_t lect;
 
-  int err=pthread_create(&lect,NULL,&lecture,(void*)*ftrad);
+  int err=pthread_create(&lect,NULL,&lecture,NULL);
   if(err!=0)
     {
       printf("erreur Lecture\n");
     }
   err = pthread_join(lect,NULL);
-   
-  free(ptr);
-  err=printf("\033[0;31mFonction principale finie\033[00m\n");
+  
+  pthread_mutex_destroy(&mutex_hash);
+  free(hash);
+  free(ftrad);
+  printf("\033[0;31mFonction principale finie\033[00m\n");
   return 1;
 }
