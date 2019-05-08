71e30d6260114ad04d402174c6476164f8aafce1
damien <damien@localhost.localdomain>
2019-05-08 21:18:40 +0200

    Commentaires

--- src/cracker.c
+++ src/cracker.c
@@ -15,28 +15,31 @@
 #include <sys/time.h>
 
 
-char **ftrad;        /*Liste des fichiers*/
-uint8_t **hash;         /*Tableau pour les pointeurs hash */
-char **trad;          /*Tableau pour les pointeurs traduction */
+char **ftrad;    /*Liste des fichiers*/
+uint8_t **hash;  /*Tableau pour les pointeurs hash */
+char **trad;     /*Tableau pour les pointeurs traduction */
 pthread_mutex_t mutex_hash;
 pthread_mutex_t mutex_trad;
 sem_t hashempty;
 sem_t hashfull;
 sem_t tradempty;
 sem_t tradfull;
-int N = 1; /* nombre de threads de calcul*/
-int M = 0; /*nombre de threads ayant fini l'étape traduction */
-int W = 0; /*nombre de threads ayant fini l'étape lecture */
-int c = 0;
-int o = 0;
-char *out;
-int count_mot;
+int N = 1;  /* nombre de threads de calcul*/
+int M = 0;  /*nombre de threads ayant fini l'étape traduction */
+int W = 0;  /*nombre de threads ayant fini l'étape lecture */
+int c = 0;  /*valeur qui indique si on analyse les consonnes */
+int o = 0;  /*valeur qui indique si il y a un fichier de sortie */
+char *out;  /*Nom fi fichier de sortie*/
+int count_mot; /*Compteur de mots a traduire*/
 
+
+/* Definition de la structure node qui contient un pointeur char et un pointeur node*/
 typedef struct node{
   struct node *next;
   char *mot;
 } node_t;
 
+/* Definition de la stucture list qui contient un pointeur vers une node qui est la première de la liste*/
 typedef struct list{
   struct node *first;
   int size;
@@ -46,8 +49,6 @@ typedef struct list{
 /*
   Fonction pour initialiser une node  contennant un mot pointé par "val"
  */
-
-
 node_t* init_node(char* val){
   node_t *n;
   n = malloc(sizeof(node_t));
@@ -80,8 +81,6 @@ int add_node(list_t *list, char* val){
 /*
   Fonction qui vide la liste (le pointeur vers la liste est conservé)
 */
-
-
 void empty_list(list_t *list){
   node_t *c = list->first;
   list->first = NULL;
@@ -98,8 +97,6 @@ void empty_list(list_t *list){
 /*
   Fonction qui suppprime la liste et son contenu
 */
-
-
 void delete_list(list_t *list){
   empty_list(list);
   free(list->first);
@@ -111,8 +108,6 @@ void delete_list(list_t *list){
   Fonction qui, pour le mot pointé par "mot", compte le nombre d'occurence de voyelles ou de consonnes en fonction du paramètre c
 
 */
-
-
 int count(char *mot, int c){
   int nbr = 0;
   for(int i = 0 ; i < strlen(mot) ; i++){
@@ -141,13 +136,13 @@ void *lecture(void *param)
   printf("il y a %i fichiers a lire\n",nf);
   count_mot =0;
 
-  for(int i=1;i<nf+1;i++){                          /*boucle pour lire tout les fichiers*/
+  for(int i=1;i<nf+1;i++){                       /*boucle pour lire tout les fichiers*/
     printf("%s\n",ftrad[i]);
     int a = open(ftrad[i],O_RDONLY);
     if(a==-1){printf("impossible d'ouvrir le fichier %i\n",i ); }
 
     int stat = 1;
-    while(stat  != 0){                              /*boucle pour lire tout les hash*/
+    while(stat  != 0){                           /*boucle pour lire tout les hash*/
       uint8_t* buff= malloc(32);
       stat = read(a,buff,32);
       if (stat==-1){
@@ -155,21 +150,21 @@ void *lecture(void *param)
 	exit(0);
       }
       else if(stat != 0){
-	for(int i =0; i==0;){                      /*boucle pour mette le hash dans le tableau */
+	for(int i =0; i==0;){                    /*boucle pour mette le hash dans le tableau */
 	  sem_wait(&hashempty);
 	  pthread_mutex_lock(&mutex_hash);
-	  for(int j = 0; j < N+1 && i==0 ;j++){    /*boucle pour chercher une place*/
+	  for(int j = 0; j < N+1 && i==0 ;j++){  /*boucle pour chercher une place*/
 	    if(hash[j]==NULL){
 	      hash[j]=buff;
 	      i=1;
 	    }
 	  }
 	  pthread_mutex_unlock(&mutex_hash);
-	}
+	}  
 	sem_post(&hashfull);
 	count_mot++;
       }
-    }
+    }  
     close(a);
   }
   printf("il y a %i mots\n",count_mot);
@@ -183,97 +178,93 @@ void *lecture(void *param)
 /*
   Fonction pour le thread de traduction
 */
-
-
 void *traduction (void *param)
 {
   int value;
   sem_getvalue(&hashfull,&value);
   uint8_t *buf;  /* buffer pour le hash*/
-  while(W != 1 || value != 0)
-  {
-      
-      for(int m =0; m==0;){                            /*Boucle pour copier un hash */
-	sem_wait(&hashfull);	
-	pthread_mutex_lock(&mutex_hash);
-	for(int n = 0; n < N+1 && m == 0 ; n++){       /*Boucle pour chercher le hash a copier*/
-	  if(hash[n] != NULL)
-	    {
-	      buf = hash[n];
-	      hash[n]=NULL;
-	      m = 1;
-	    }
-	}
-	pthread_mutex_unlock(&mutex_hash);
+  while(W != 1 || value != 0){                     /*Boucle principale qui agit tant que la lecture n'est pas finie ou que le tableau hash n'est pas vide */
+    for(int m =0; m==0;){                          /*Boucle pour copier un hash */
+      sem_wait(&hashfull);	
+      pthread_mutex_lock(&mutex_hash);
+      for(int n = 0; n < N+1 && m == 0 ; n++){     /*Boucle pour chercher le hash a copier*/
+	if(hash[n] != NULL)
+	  {
+	    buf = hash[n];
+	    hash[n]=NULL;
+	    m = 1;
+	  }
       }
-      sem_post(&hashempty);
-      char *buf2 = malloc(16); /* buffer pour la traduction*/
-      reversehash(buf,buf2,16);
-      free(buf); 
-
-      for(int i =0; i==0;){
-	sem_wait(&tradempty);
-	pthread_mutex_lock(&mutex_trad);
-	for(int j = 0; j < N+1 && i==0 ; j++){
-	  if(trad[j]==NULL){
+      pthread_mutex_unlock(&mutex_hash);
+    }
+    sem_post(&hashempty);
+    char *buf2 = malloc(16);/* buffer pour la traduction*/
+    reversehash(buf,buf2,16);
+    free(buf);  /*le pointeur du hash plus nécessaire*/
+    
+    for(int i =0; i==0;){                        /*Boucle pour mettre le mot dans le tableau*/
+      sem_wait(&tradempty);
+      pthread_mutex_lock(&mutex_trad);
+      for(int j = 0; j < N+1 && i==0 ; j++){     /*Boucle pour chercher une place*/
+	if(trad[j]==NULL){
 	    trad[j]=buf2;
 	    i=1;
-	  }
 	}
-	pthread_mutex_unlock(&mutex_trad);
       }
-      sem_post(&tradfull);
-      sem_getvalue(&hashfull,&value);
+      pthread_mutex_unlock(&mutex_trad);
+    }
+    sem_post(&tradfull);
+    sem_getvalue(&hashfull,&value);
   }
   printf("fin de traduction\n");
   M++;
   pthread_exit(NULL);
 }
 
+
 /*
   Fonction pour le thread qui choisi les candidats
 */
-
-
 void *candidat(void* param)
 {
-  int value;
+  int value; /*Mots restants dans de tableau de mots traduits*/
   sem_getvalue(&tradfull,&value);
-  list_t *list = malloc(sizeof(list_t));
-  char *tra;
-  int nbr = 0;
-  int co;
-  int counter = 0;
-  while(counter < count_mot || W != 1)
-    {
-      for(int m =0; m==0;){
-	sem_wait(&tradfull);
-	pthread_mutex_lock(&mutex_trad);
-	for(int n = 0; n < N+1 && m == 0 ; n++){
-	  if(trad[n] != NULL){
-	    tra = trad[n];
-	    trad[n]=NULL;
-	    m = 1;
-	  }
+  list_t *list = malloc(sizeof(list_t)); /*Initialisation de la liste*/
+  char *tra;   /*mot courrant analysé*/
+  int nbr = 0; /*nombre d'occurence maximal de consonne ou voyelle */
+  int co;      /*Nombre d'occucence du mot actuellement analysé */
+  int counter = 0; /*Compteur de mots traduits*/
+  while(counter < count_mot || W != 1){        /*Boucle principale qui agit tant que le conteur de mots traduits est plus petit que le nombre de mots lu ou que la lecture n'est pas finie */
+    for(int m =0; m==0;){                      /*Boucle pour prendre un mot*/
+      sem_wait(&tradfull);
+      pthread_mutex_lock(&mutex_trad);
+      for(int n = 0; n < N+1 && m == 0 ; n++){ /*Boucle pour chercher un mot dans le tableau*/
+	if(trad[n] != NULL){
+	  tra = trad[n];
+	  trad[n]=NULL;
+	  m = 1;
 	}
-	pthread_mutex_unlock(&mutex_trad);
-      }	
-      sem_post(&tradempty);
-      co = count(tra,c);
-      if(co == nbr){
-	add_node(list,tra);
       }
-      else if(co > nbr){
-	nbr = co;
-	empty_list(list);
-	add_node(list,tra);
-      }
-      else{
-	free(tra);
-      }
-      sem_getvalue(&tradfull,&value);
-      counter++;
+      pthread_mutex_unlock(&mutex_trad);
+    }	
+    sem_post(&tradempty);
+    /*Voir si c'est un candidat*/
+    co = count(tra,c);
+    if(co == nbr){
+      add_node(list,tra);
     }
+    else if(co > nbr){
+      nbr = co;
+      empty_list(list);
+      add_node(list,tra);
+    }
+    else{
+      free(tra);
+    }
+    sem_getvalue(&tradfull,&value);
+    counter++;
+  }
+  /*Choix des candidats terminé*/
   if(o)
     {
       int a = open(out,O_RDWR | O_TRUNC);
@@ -287,16 +278,14 @@ void *candidat(void* param)
 	}
       close(a);
     }   
-  else
-    {
+  else{
     struct node *n = list->first;
-    for (int i = 0; i < list->size ; i++)
-      {
-	printf(n->mot);
-	printf("\n");
-	n = n->next;
-      }
+    for (int i = 0; i < list->size ; i++){
+      printf(n->mot);
+      printf("\n");
+      n = n->next;
     }
+  }
   delete_list(list);
   printf("fin de la sélection\n");
   pthread_exit(NULL);
@@ -306,62 +295,49 @@ void *candidat(void* param)
 /*
   Fonction principale
  */
-
-
-int main(int argc,char *argv[])
-{
+int main(int argc,char *argv[]){
   struct timeval tv1,tv2;
   long long temps;
   gettimeofday(&tv1,NULL);
   int opt;
   int f = 1;
-
-  while ((opt = getopt (argc, argv, "t:co:")) != -1)
-  {
-	if(opt == 't')
-	{
-		N = atoi(optarg);
-	}
-	if(opt == 'c')
-	{
-		c = 1;
-	}
-	if(opt == 'o')
-	{
-		o = 1;
-		out = optarg;
-	}
-	f = optind;
+  while ((opt = getopt (argc, argv, "t:co:")) != -1){
+    if(opt == 't'){
+      N = atoi(optarg);
+    }
+    if(opt == 'c'){
+      c = 1;
+    }
+    if(opt == 'o'){
+      o = 1;
+      out = optarg;
+    }
+    f = optind;
   }	
-
-  int nf = argc-f;
-
+  
+  int nf = argc-f; /*Le nobre de fichiers a traduire*/
+  
 
   /*
     Fin de L'anayse des arguments
    */
-
-
   ftrad=  malloc(sizeof(char*)*(nf+1));
   ftrad[0] = (char*)&nf;
-
-  for(int i = 1; i<nf+1 ;i++)
-    {
-      ftrad[i]=argv[f+i-1];
-    }
+  
+  for(int i = 1; i<nf+1 ;i++){
+    ftrad[i]=argv[f+i-1];
+  }
   
   hash = calloc((N+1),8);
-  if(hash[0] != NULL)
-    {
-      printf("\terreur dans la creation du tableau de hash\n");
-    }
+  if(hash[0] != NULL){
+    printf("\terreur dans la creation du tableau de hash\n");
+  }
 
   trad = calloc((N+1),8);
-  if(trad[0]!= NULL)
-    {
-      printf("\terreur dans la creation du tableau de traduction\n");
-    }
-
+  if(trad[0]!= NULL){
+    printf("\terreur dans la creation du tableau de traduction\n");
+  }
+  
   pthread_mutex_init(&mutex_hash,NULL);
   pthread_mutex_init(&mutex_trad,NULL);
   sem_init(&hashfull,0,0);
@@ -373,39 +349,31 @@ int main(int argc,char *argv[])
   /*
     Etape de création des threads
    */
-
   pthread_t lect;
   int err=pthread_create(&lect,NULL,&lecture,NULL);
-  if(err!=0)
-  {
+  if(err!=0){
     printf("impossible de creer le thread de lecture \n");
   }
-
+  
   pthread_t threads[N];
-  for(int i=0; i < N ; i++)
-  {
-    if (pthread_create(&(threads[i]),NULL,&traduction,NULL) != 0)
-        {
-          printf("impossible de creer le thread de traduction %i \n", i);
-        }
+  for(int i=0; i < N ; i++){
+    if (pthread_create(&(threads[i]),NULL,&traduction,NULL) != 0){
+      printf("impossible de creer le thread de traduction %i \n", i);
+    }
   } 
 
   pthread_t selec;
   int errrr=pthread_create(&selec,NULL,&candidat,NULL);
-  if(errrr!=0)
-    {
-      printf("impossible de creer le thread de selection \n");
+  if(errrr!=0){
+    printf("impossible de creer le thread de selection \n");
     }
-
+  
    /*
     Etape de lancement des threads
    */
-
- 
   pthread_join(lect,NULL);
-  for(int j = 0 ; j < N ; j++)
-  {
-     pthread_join(threads[j], NULL);
+  for(int j = 0 ; j < N ; j++){
+    pthread_join(threads[j], NULL);
   }
   pthread_join(selec,NULL);
 
@@ -413,8 +381,6 @@ int main(int argc,char *argv[])
    /*
     Etape de destruction des threads
    */
-
-
   pthread_mutex_destroy(&mutex_hash);
   pthread_mutex_destroy(&mutex_trad);
   sem_destroy(&hashfull);
